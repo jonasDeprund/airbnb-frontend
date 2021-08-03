1b5ed56ee190e535c150af68adbdd4a4ae76a925
Jonas Deprund <jonasdeprund@gmail.com>
2021-08-03 14:38:43 +0200

    linked done

--- pages/property/[slug].js
+++ pages/property/[slug].js
@@ -3,7 +3,7 @@ import { isMultiple } from '../../utils';
 import Image from '../../components/Image';
 // import Review from '../../components/Review';
 import Map from '../../components/Map';
-// import Link from 'next/link';
+import Link from 'next/link';
 
 const Property = ({
   title,
@@ -73,7 +73,9 @@ const Property = ({
         <div className="price-box">
           <h2>£{pricePerNight}</h2>
           <h4>{/* {reviewAmount} review{isMultiple(reviewAmount)} */}</h4>
-          <div className="button">Change Dates</div>
+          <Link href="/">
+            <div className="button">Change Dates</div>
+          </Link>
         </div>
       </div>
 
